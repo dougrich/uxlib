0c96ffa4389cc06a1799b319b83b7b4995f6db3d
Douglas Richardson <dougrich@outlook.com>
2019-08-08 10:21:55 -0700

    Re-arranged components

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from '@dougrich/uxlib'
+import { ExampleComponent } from '@dougrich/uxlib'
 
 export default class App extends Component {
   render () {
